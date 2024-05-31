7207c8087c0469578cb5ebff32118aa2c96a644a
leejicircle <leeji.circle@gmail.com>
2024-05-31 20:33:13 +0900

    검색 버튼 모바일 데스크탑 설정

--- js/main.js
+++ js/main.js
@@ -40,11 +40,15 @@ const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', function(event) {
+  event.stopPropagation
+  hideSearch()
+})
 searchshadowEl.addEventListener('click', hideSearch)
+
 function showSearch() {
   headerEl.classList.add('searching')
-  document.documentElement.classList.add('fixed')
+  stopScroll()
   delaysetting(headerMenuEl.reverse())
   delaysetting(searchDelayEls)
  
@@ -55,13 +59,21 @@ function showSearch() {
 }
 function hideSearch() {
   headerEl.classList.remove('searching')
-  document.documentElement.classList.remove('fixed')
+  playScroll()
   delaysetting(headerMenuEl.reverse())
   delaysetting(searchDelayEls.reverse())
   searchDelayEls.reverse()
   
   searchInputEl.value = ''
 }
+
+function playScroll () {
+  document.documentElement.classList.remove('fixed')
+}
+function stopScroll () {
+  document.documentElement.classList.add('fixed')
+}
+
 // function Menudelay (){
 //   this.forEach (function(el, index) {
 //     el.style.transitionDelay = index * .4 / headerMenuEl.length + 's'
@@ -78,6 +90,42 @@ function delaysetting (elements) {
   })
 
 }
+
+
+
+// 헤더 메뉴 토글
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
+    playScroll()
+  } else {
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+})
+
+// 헤더 검색
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', function ()  {
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile')
+})
+
+// 
+window.addEventListener('resize', function () {
+  if(this.window.innerWidth<=740) {
+    headerEl.classList.remove('searching')
+  }else {
+    headerEl.classList.remove('searching--mobile')
+  }
+})
+
 
 // 요소의 가시성 관창
 const io = new IntersectionObserver(function (entries) {
